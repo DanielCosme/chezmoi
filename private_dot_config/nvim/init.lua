4539cb50f79e37a8a96308982089fc0e586bc455
Daniel Cosme <cosmedaniel8@gmail.com>
2022-07-24 19:02:52 -0400

    Add telescope

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -17,7 +17,7 @@ local lsp_defaults = {
     ),
     -- Callback function that will be executed when a language server is attached to a buffer.
     on_attach = function(client, bufnr)
-        print("hello lsp")
+        print("Hello lsp")
         vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
     end
 }
@@ -67,5 +67,115 @@ lspconfig.gopls.setup({
     -- on_attach = function(client, bufnr)
     --     lspconfig.util.default_config.on_attach(client, bufnr)
     -- end
+})
+
+-- Diagnostics
+vim.diagnostic.config({
+  virtual_text = false,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
+})
+
+
+-- Lsp windows with borders
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {border = 'rounded'}
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {border = 'rounded'}
+)
+
+-- Snippet Engine Configuration
+require('luasnip.loaders.from_vscode').lazy_load()
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    sources = {
+        {name = 'path'}, -- autocomplete paths
+        {name = 'nvim_lsp', keyword_length = 3}, -- shows suggestions based on the language server response
+        {name = 'buffer', keyword_length = 3}, -- suggests words found in the current buffer
+        {name = 'luasnip', keyword_length = 2}, -- shows available snippets and expands them if they are chosen
+    },
+    window = {
+        documentation = cmp.config.window.bordered()
+    },
+    formatting = {
+        fields = {'menu', 'abbr', 'kind'}
+    },
+    format = function(entry, item)
+        local menu_icon = {
+            nvim_lsp = 'λ',
+            luasnip = '⋗',
+            buffer = 'Ω',
+            path = '🖫',
+        }
+
+        item.menu = menu_icon[entry.source.name]
+        return item
+    end,
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-k>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<C-j>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.abort(),
+        -- Jump to the next placeholder in the snippet.
+        ['<C-n>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+        -- Jump to the previous placeholder in the snippet.
+        ['<C-p>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+        -- Autocomplete with tab.
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          local col = vim.fn.col('.') - 1
+
+          if cmp.visible() then
+            cmp.select_next_item(select_opts)
+          elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+            fallback()
+          else
+            cmp.complete()
+          end
+        end, {'i', 's'}),
+        -- If the completion menu is visible, move to the previous item.
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item(select_opts)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+
+    }
 })
 
--- private_dot_config/nvim/lua/danicosme/packer.lua
+++ private_dot_config/nvim/lua/danicosme/packer.lua
@@ -6,6 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   -- Packer can manage itself
 	use 'wbthomason/packer.nvim'
+    -- Lsp & autocomplete
     use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
     use 'L3MON4D3/LuaSnip'
     use "rafamadriz/friendly-snippets"
@@ -13,7 +14,19 @@ return require('packer').startup(function()
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/nvim-cmp'
+    -- Colors
 	use 'folke/tokyonight.nvim'
+    -- Finder
+    use {'nvim-treesitter/nvim-treesitter', 
+        run = ':TSUpdate'
+    }
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    -- Other
+    use 'nvim-lua/plenary.nvim'
     -- Nvim Tree
     use 'kyazdani42/nvim-web-devicons'
     use {
