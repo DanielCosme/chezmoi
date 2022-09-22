2fb015c935de0af2e0d753000875ee24053f8343
Daniel Cosme <cosmedaniel8@gmail.com>
2022-09-21 20:41:50 -0400

    Add context to neovim

--- private_dot_config/nvim/lua/danicosme/packer.lua
+++ private_dot_config/nvim/lua/danicosme/packer.lua
@@ -56,5 +56,7 @@ return require('packer').startup(function()
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    -- Context
+    use 'nvim-treesitter/nvim-treesitter-context'
 end)
 
