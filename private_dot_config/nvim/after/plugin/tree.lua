6a52b0217c5bd39b66e57011ae7f93323603739f
Daniel Cosme <cosmedaniel8@gmail.com>
2023-03-03 20:38:32 -0500

    Update autostart tree config

--- private_dot_config/nvim/after/plugin/tree.lua
+++ private_dot_config/nvim/after/plugin/tree.lua
@@ -14,8 +14,3 @@ require("nvim-tree").setup({
 
 vim.keymap.set('n', '<C-n>', ":NvimTreeToggle<CR>")
 vim.keymap.set('n', "<leader>gn", ":NvimTreeFindFile<CR>")
-
-local function open_nvim_tree()
-  require("nvim-tree.api").tree.open()
-end
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
