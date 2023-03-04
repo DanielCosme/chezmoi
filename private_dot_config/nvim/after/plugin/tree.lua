16f01f8ae920de29057e4c67e263bd1a2302b2f8
Daniel Cosme <cosmedaniel8@gmail.com>
2023-03-03 20:33:04 -0500

    Update neovim

---
+++ private_dot_config/nvim/after/plugin/tree.lua
@@ -0,0 +1,21 @@
+-- Disable netrw to avoid race conditions with nvim-tree
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set('n', '<C-n>', ":NvimTreeToggle<CR>")
+vim.keymap.set('n', "<leader>gn", ":NvimTreeFindFile<CR>")
+
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,11 @@
+require('daniel')
+
+vim.diagnostic.config {
+    severity_sort = true,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+}
