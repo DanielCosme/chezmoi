4fa200da95b0695c2f65b5c20341f1bba993ca32
Daniel Cosme <cosmedaniel8@gmail.com>
2023-04-18 07:02:14 -0400

    Add git blame to neovim

--- private_dot_config/nvim/lua/daniel/lazy.lua
+++ private_dot_config/nvim/lua/daniel/lazy.lua
@@ -58,6 +58,7 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',  opts = {} },
+  'f-person/git-blame.nvim',
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
